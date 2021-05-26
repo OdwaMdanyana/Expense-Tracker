74522f5c3667d2dde186546cdf381f810f47f644
Odwa Mdanyana <mdakszin@gmail.com>
2021-05-26 18:13:58 +0200

    made minor changes to the events

--- src/components/NewExpanse/ExpanseForm.js
+++ src/components/NewExpanse/ExpanseForm.js
@@ -1,30 +1,52 @@
-import React from 'react'
-import '../NewExpanse/ExpanseForm.css'
+import React, { useState } from "react";
+import "../NewExpanse/ExpanseForm.css";
 
 function ExpanseForm() {
-    return (
-        <div>
-            <form>
-                <div className='new-expense__controls'>
-                    <div className='new-expense__control'>
-                        <label>Title</label>
-                        <input type='text'/>
-                    </div>
-                    <div className='new-expense__control'>
-                        <label>Amount</label>
-                        <input type='number' min='0.01' step='0.01'/>
-                    </div>
-                    <div className='new-expense__control'>
-                        <label>Date</label>
-                        <input type='date' min='2021-05-26' />
-                    </div>
-                </div>
-                <div className='new-expense__actions'>
-                    <button type='submit'>Add Expense</button>
-                </div>
-            </form>
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+const [] = useState({
+  enteredDate: '',
+  enteredTitle:'',
+  enteredAmount: '',
+})
+  const titleChangeHandler = (e) => {
+    //setEnteredTitle(e.target.value);
+  };
+  const amountChangeHandler = (e) => {
+    //setEnteredAmount(e.target.value);
+  };
+  const dateChangeHandler = (e) => {
+    //setEnteredDate(e.target.value);
+  };
+  return (
+    <div>
+      <form>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input type="text" onChange={titleChangeHandler} />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input type="date" min="2021-05-26" onChange={dateChangeHandler} />
+          </div>
         </div>
-    )
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default ExpanseForm
+export default ExpanseForm;
