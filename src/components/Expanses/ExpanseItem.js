2a91dedfbe96a66dc77aeed79c63387f2387ae81
Odwa Mdanyana <mdakszin@gmail.com>
2021-05-26 14:57:52 +0200

    state management in expeanseItems

--- src/components/Expanses/ExpanseItem.js
+++ src/components/Expanses/ExpanseItem.js
@@ -1,17 +1,24 @@
 import ExpanseDate from "./ExpanseDate";
 import Card from "../UI/Card";
 import "./ExpanseItem.css";
+import React, { useState } from "react";
 
 const ExpanseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
   return (
     <Card className="expense-item">
       <ExpanseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpanseItem;
